96631878f1cbf315ec0de40c8d769abed578e233
kiwik98 <kiwik9811@gmail.com>
2023-06-30 22:09:42 +0900

    String,Set의 메소드를 이용한 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,11 +3,36 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] values = "1,2".split(",");
+        int length = values.length;
+        assertThat(length).isEqualTo(2);
+    }
+
+    @Test
+    void substring() {
+        String st = "(1,2)";
+        assertThat(st.substring(1, st.length() - 1)).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt() {
+        String ch = "abc";
+        assertThat(ch.charAt(1)).isEqualTo('b');
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    ch.charAt(3);
+                }).withMessageContaining("%d", 3);
     }
 }
