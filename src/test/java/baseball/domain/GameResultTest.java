10c19da780a7d2c6e9f81beb7bcab983abef700e
kiwik98 <kiwik9811@gmail.com>
2023-07-16 21:58:28 +0900

    Test: GameResult 테스트 코드 작성

---
+++ src/test/java/baseball/domain/GameResultTest.java
@@ -0,0 +1,31 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameResultTest {
+    @Test
+    void isNothing() {
+        boolean result = GameResult.isNothing(new BaseballCount(0, 0));
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void isOnlyStrike() {
+        boolean result = GameResult.isOnlyStrike(new BaseballCount(1, 0));
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void isOnlyBall() {
+        boolean result = GameResult.isOnlyBall(new BaseballCount(0, 1));
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void isFairResult() {
+        boolean result = GameResult.isFairResult(new BaseballCount(1, 1));
+        assertThat(result).isTrue();
+    }
+}
